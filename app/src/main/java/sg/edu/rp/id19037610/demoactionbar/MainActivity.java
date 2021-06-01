e0d6093cc0fa40653f729a07985d2d6d3c3ee31a
19037610 <19037610@myrp.edu.sg>
2021-06-01 09:59:15 +0800

    completed

--- app/src/main/java/sg/edu/rp/id19037610/demoactionbar/MainActivity.java
+++ app/src/main/java/sg/edu/rp/id19037610/demoactionbar/MainActivity.java
@@ -3,6 +3,7 @@ package sg.edu.rp.id19037610.demoactionbar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.options, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
